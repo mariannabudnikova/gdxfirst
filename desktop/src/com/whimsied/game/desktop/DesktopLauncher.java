17ff643c95ad8201a40619ba860a3e09f7e8be71
Marianna <marianna.budnikova@gmail.com>
2015-04-26 21:43:03 -0600

    Initial commit

---
+++ desktop/src/com/whimsied/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.whimsied.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.whimsied.game.MyFirstGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyFirstGdxGame(), config);
+	}
+}
